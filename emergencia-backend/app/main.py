6b24c8a6c1bc07fa2117c0a510c2f8d7d8c670b3
londostalyn-byte <londo.stalyn@gmail.com>
2026-05-15 21:15:43 -0500

    Update  port main.py

--- emergencia-backend/app/main.py
+++ emergencia-backend/app/main.py
@@ -86,7 +86,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "app.main:app",
         host="0.0.0.0",
-        port=int(os.getenv("PORT", 8000)),
+        port=int(os.getenv("PORT", 5000)),
         reload=False,
         reload_excludes=["logs/*", "*.log", "*.db", "logs/"],
     )
